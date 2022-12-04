64e6988d16deb832b315e8e624302de8db0863ab
babykiss2 <119665992+babykiss2@users.noreply.github.com>
2022-12-05 01:54:28 +0900

    Add files via upload

--- weather.js
+++ weather.js
@@ -5,9 +5,13 @@ function GeoSuccess(position){
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
     fetch(url).then(response => response.json().then(data => {
+        const divWeather = document.querySelector("#weather");
+        const i = document.createElement("i");
+        i.className = "bi bi-cloud-fill";
+        divWeather.prepend(i);
         const weather = document.querySelector("#weather span:last-child");
-        const city = document.querySelector("#weather span:first-child");
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}°`;
+        const city = document.querySelector("#weather span:nth-child(2)");
+        weather.innerText = `  ${data.weather[0].main} / ${data.main.temp}°`;
         city.innerText = `${data.name} : `;
     }));
 }
